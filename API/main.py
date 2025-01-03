aa2f211d839393d9813db41065f38c8d6178d776
TulupovKirill <lolopo3457@gmail.com>
2025-01-03 17:59:38 +0600

    create rest

--- API/main.py
+++ API/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
+from ..hermes import talk_with_hermes
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return 200
-
-
 @app.get("/rag/get")
-async def get_response():
-    return 200
+async def get_response(query):
+    response = talk_with_hermes(query)
+    return response
--- hermes.py
+++ hermes.py
@@ -2,9 +2,9 @@ import chromadb
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM
 
-KOSMO_CHROMA_FOLDER = "PATH"
+KOSMO_CHROMA_FOLDER = "kosmo_chroma2"
 
-assert KOSMO_CHROMA_FOLDER != "PATH", "specify correct path"
+# assert KOSMO_CHROMA_FOLDER != "kosmo_chroma2", "specify correct path"
 
 chroma_client = chromadb.PersistentClient(path=KOSMO_CHROMA_FOLDER)
 
