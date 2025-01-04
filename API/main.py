746dcc43fc9f9a080ff377e23e2b229d22bd52e0
TulupovKirill <lolopo3457@gmail.com>
2025-01-05 00:40:43 +0600

    secret url because he random

--- API/main.py
+++ API/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/rag/get")
 async def get_response(query):
-    response = requests.get("https://3065-35-230-38-63.ngrok-free.app", params={"query": query})
+    response = requests.get("URL", params={"query": query})
     return response.json()
